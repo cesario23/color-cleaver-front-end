9ad8ba9d83db7fa01b51930102a11e95f0524d59
cesar juarez <cesar.juarez@codeimmersives.com>
2021-02-27 23:31:54 -0800

    color-cleaver

--- color-cleaver.js
+++ color-cleaver.js
@@ -5,3 +5,18 @@ const isValidSecondary = require("./is-valid-secondary.js");
 const getInput = require("./get-input.js");
 
 // Your code here!
+
+const color1 = getInput (1);
+const color2 = getInput (2);
+
+if (!isValidPrimary (color1) && !isValidSecondary (color2)){
+    console.log ("we need at least one color")
+}
+if (isValidPrimary(color1) || isValidSecondary (color2)){
+    return colorDeconstructor;
+    console.log ("your colors are" + colorDeconstructor)
+}
+if (isValidPrimary (color1) && isValidSecondary (color2)){
+    return colorCombinator;
+    console.log ("your color is:" + colorCombinator)
+}
